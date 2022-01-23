0646e9275db0c3d67ef1eadab6b7909d5d21407f
= <=>
2022-01-23 22:28:05 +0700

    fix: card component error

--- App.tsx
+++ App.tsx
@@ -7,10 +7,10 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 export default function App() {
   return (
       // <ImageViewScreen />
-      <WelcomeScreen />
-      // <View style={{backgroundColor: '#f8f4f4', paddingHorizontal: 20, paddingVertical: 100}}>
-      //   <Card title='Read jacket for sale!' subTitle='$100'  />
-      // </View>
+      // <WelcomeScreen />
+      <View style={{backgroundColor: '#f8f4f4', paddingHorizontal: 20, paddingVertical: 100}}>
+        <Card title='Read jacket for sale!' subTitle='$100' imageSource={require("./assets/images/jacket.jpg")} />
+      </View>
   );
 }
 
--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -5,16 +5,18 @@ import colors from '../config/colors';
 interface Props{
   title: string;
   subTitle: string;
-
+  imageSource: any;
 }
 
-const Card = ({title, subTitle}: Props) => {
+const Card = ({title, subTitle, imageSource}: Props) => {
   
   return (
       <View style={styles.cardContainer}>
-        
-        <Text>{title}</Text>
-        <Text>{subTitle}</Text>
+        <Image source={imageSource} style={styles.image}></Image>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.subTitle}>{subTitle}</Text>
+        </View>
     </View>
   );
 };
@@ -24,16 +26,23 @@ export default Card;
 const styles = StyleSheet.create({
   cardContainer: {
     backgroundColor: colors.white,
+    borderRadius: 15,
+    overflow: 'hidden',
   },
   image:{
     width: '100%',
-    height: 100,
-
+    height: 200,
+  }, 
+  titleContainer:{
+    padding: 20,
   },
   title: {
-
+    fontSize:18,
+    marginBottom: 10,
   },
   subTitle:{
-
+    fontSize:18,
+    color: colors.secondary,
+    fontWeight: 'bold'
   }
 });
